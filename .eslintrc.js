c12e06dfb7024b054266ceaaf368e5d78e9462c4
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-11-12 01:55:41 +0530

    feat: init 6 🥳

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "node": true
+  },
+  "extends": [
+    "plugin:vue/vue3-strongly-recommended",
+    "eslint:recommended"
+  ],
+  "parserOptions": {
+    "parser": "babel-eslint"
+  },
+  "rules": {}
+};
