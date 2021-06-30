d2353c5dffc8db232583ed922d2b28ccd4411c46
Jerome Wu <jeromewus@gmail.com>
2021-06-30 15:55:16 +0800

    fix: core is undefined

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const path = require('path');
+const express = require('express');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-app/'
-    : '/'
+    : '/',
+  configureWebpack: {
+    devServer: {
+      before: app => {
+        app.use('/node_modules/', express.static(path.resolve(__dirname, 'node_modules')))
+      }
+    }
+  }
 };
